d3934071965e0baa48b037547dc157ae553f2733
Arun Narayanan <arun.narayanan@expedia.com>
2020-03-05 09:39:29 +0530

    Initial Commit

---
+++ src/main/java/com/spring/handson/model/User.java
@@ -0,0 +1,80 @@
+package com.spring.handson.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class User {
+
+
+    @Id
+    private String id;
+
+    private String firstName;
+    private String lastName;
+
+
+    private String username;
+    @JsonIgnore
+    private String password;
+    private long salary;
+    private int age;
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public long getSalary() {
+        return salary;
+    }
+
+    public void setSalary(long salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/spring/handson/service/UserService.java
@@ -0,0 +1,19 @@
+package com.spring.handson.service;
+
+import com.spring.handson.model.User;
+import com.spring.handson.model.UserDto;
+
+import java.util.List;
+
+public interface UserService {
+
+    User save(UserDto user);
+    List<User> findAll();
+    void delete(int id);
+
+    User findOne(String username);
+
+    User findById(int id);
+
+    UserDto update(UserDto userDto);
+}
